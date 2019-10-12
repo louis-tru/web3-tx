2c876bb0a918af0194b5436fca58f4560804e829
louis <louistru@hotmail.com>
2019-10-12 15:32:28 +0800

    up errno

--- errno.js
+++ errno.js
@@ -7,4 +7,5 @@ module.exports = {
 	ERR_PREV_TRANSACTION_NO_COMPLETE: [100222, '设备繁忙请稍后再试', '上一笔交易未完成'],
 	ERR_ETH_TRANSACTION_FAIL: [100241, '发送以太坊交易失败'],
 	ERR_REQUEST_TIMEOUT: [100232, '请求操时', '请求操时'],
+	ERR_TRANSACTION_TIMEOUT: [100256, 'TRANSACTION TIMEOUT'],
 };
