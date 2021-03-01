b13fbf80d399b5a903b8082188674d425e827256
louistru <louistru@hotmail.com>
2021-03-01 10:09:52 +0800

    up:

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ import utils from 'somes';
 import buffer, {IBuffer} from 'somes/buffer';
 import errno from './errno';
 import './_fix_contract';
-import Web3 from 'web3';
+import __Web3__ from 'web3';
 import * as net from 'net';
 import {Contract as ContractRaw, Options as ContractOptions, 
 	EventData, CallOptions, SendOptions, ContractSendMethod as ContractSendMethodRaw } from 'web3-eth-contract';
@@ -41,7 +41,7 @@ import {BlockTransactionString as Block} from 'web3-eth';
 
 import './_fix_web3';
 
-const __Web3__ = require('web3') as typeof Web3;
+const Web3 = require('web3') as typeof __Web3__;
 
 export { Web3, ContractOptions, EventData, Transaction, TransactionReceipt, Block, CallOptions, SendOptions };
 
@@ -52,8 +52,6 @@ const TRANSACTION_MAX_BLOCK_RANGE = 32;
 const TRANSACTION_CHECK_TIME = 1e4; // 10秒
 const DEFAULT_GAS_LIMIT = 1e8;
 const DEFAULT_GAS_PRICE = 1e5;
-
-exports.Web3 = __Web3__;
 
 export interface FindEventResult {
 	event: EventData;
@@ -109,7 +107,7 @@ export interface SerializedTx {
 }
 
 export interface IWeb3Z {
-	readonly web3: Web3;
+	readonly web3: __Web3__;
 	readonly gasLimit: number;
 	readonly gasPrice: number;
 	getDefaultAccount(): Promise<string>;
@@ -178,7 +176,7 @@ class TransactionPromiseIMPL extends utils.PromiseNx<TransactionReceipt> impleme
 export class Web3Z implements IWeb3Z {
 	private _gasLimit = DEFAULT_GAS_LIMIT;
 	private _gasPrice = DEFAULT_GAS_PRICE;
-	private _web3?: Web3;
+	private _web3?: __Web3__;
 
 	TRANSACTION_CHECK_TIME = TRANSACTION_CHECK_TIME;
 
@@ -189,7 +187,7 @@ export class Web3Z implements IWeb3Z {
 	get web3() {
 		if (!this._web3) {
 			var provider = this.getProvider();
-			var { HttpProvider, WebsocketProvider, IpcProvider } = __Web3__.providers;
+			var { HttpProvider, WebsocketProvider, IpcProvider } = Web3.providers;
 			if (typeof provider == 'string') {
 				if (/^https?:/.test(provider)) { // http
 					provider = new HttpProvider(provider, { timeout: SAFE_TRANSACTION_MAX_TIMEOUT });
@@ -201,9 +199,9 @@ export class Web3Z implements IWeb3Z {
 					throw Error(`Can't create 'Web3 provider`);
 				}
 			}
-			this._web3 = new __Web3__(provider);
+			this._web3 = new Web3(provider);
 		}
-		return this._web3 as Web3;
+		return this._web3 as __Web3__;
 	}
 
 	setDefaultAccount(account: string) {
